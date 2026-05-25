456f7f410a4afb8f7fb4c0ef7bb6e2af962bbb9c
Gimmy061 <gir.izzo@studenti.unina.it>
2026-05-25 12:16:44 +0200

    gui

---
+++ src/main/java/gui/STUDENTE.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class STUDENTE {
+    private JTextArea benvenutoStudenteQuiTroveraiTextArea;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
